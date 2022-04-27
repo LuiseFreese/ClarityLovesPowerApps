139e51e11fb7f8cbde8b61b2997fff382078189a
Luise Freese <luise@raeuberleiterin.de>
2022-04-27 17:26:56 +0200

    Update index.ts
    :-)

--- ClarityCodeSnippet/index.ts
+++ ClarityCodeSnippet/index.ts
@@ -22,7 +22,7 @@ private _Container: HTMLDivElement;
     {
         const head = document.getElementsByTagName('head')[0];
         const script = document.createElement('script');
-        script.innerHTML = '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "bihyolfhem");'
+        script.innerHTML = '(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "<your clarity project id>");'
         head.insertBefore(script, head.firstChild);
         const textDiv = "<div>We track this app with Microsoft Clarity</div>"
        
